29a2f9b7e3af05187e9c23f8a195c9a03e736219
saves7 <a_v.efimov@mail.ru>
2021-12-04 18:56:29 +0300

    Подсчет фильмов

--- js/main.js
+++ js/main.js
@@ -5,16 +5,33 @@ console.log(numberOfFilm);
 
 
 // 2 создать объект и поместить ответ 1 вопроса, а так же пустые объекты
+
+
+
+//3 поместить  в объек movies два ответа: фильм и рейтинг;
+let a = prompt('Фильм');
+let b = prompt('Рейтинг');
+let c = prompt('Фильм');
+let d = prompt('Рейтинг');
+
 let personalMovieDb = {
     count: numberOfFilm,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-
 };
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[b] = c;
+
+console.log (personalMovieDb);
+
+
+
+
+
 
 // 3 задать 2 раза вопросы: посмотренный фильм и его рейтинг, ответы в 
 //в movies так: movies: { 'logan' : '8.1'}
 
-let movQwest = function(mov, rang){};
