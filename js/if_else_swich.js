b7429588308434490451a948bd3d4d7b7fefe451
saves7 <a_v.efimov@mail.ru>
2021-12-05 09:16:42 +0300

    Добавлен if else switch и тернарный оператор ?

---
+++ js/if_else_swich.js
@@ -0,0 +1,13 @@
+
+(true) ? console.log("Выражение верно") : console.log("Выражение не верно");
+
+
+let a =10;
+switch (a){
+    case 10 : console.log('yes');
+    break;
+    case 11 : console.log('no');
+    break;
+    default : console.log('no');
+    break;
+}
